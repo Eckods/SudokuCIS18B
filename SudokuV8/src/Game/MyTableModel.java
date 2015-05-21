6f605543b70104b9bedb1bde8320a84a03132d37
Eckods <eckods@hotmail.com>
2015-05-21 16:29:19 -0700

    Pushing work we did in class. Code still needs clean-up. Restart button after hint issue needs fixing.
    Signed-off-by: Eckods <eckods@hotmail.com>

---
+++ SudokuV8/src/Game/MyTableModel.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * MyTableModel represents a new model for the Sudoku Table to follow,
+ * mainly existing to restrict what cells can be editable
+ * @author Steve Sanchez
+ */
+public class MyTableModel extends AbstractTableModel{
+    private final String forbidden[][] = new String[9][9];
+    public final Object rowData[][] = new Object[9][9];
+    private final Object colData[] = new Object[9];
+    private int count = 0;
+    
+    /**
+     * Initializes the forbidden cells, rowData, and colData for the 
+     * Table model
+     * @param rowValues The row data
+     * @param colValues The column data
+     * @param forbiddenCells The forbidden cells of the table
+     */
+    public MyTableModel(Object rowValues[][], Object colValues[], String forbiddenCells[][], int pressed){
+        for(int i = 0; i <9; i++){
+            for(int j = 0; j < 9; j++){
+                forbidden[i][j] = forbiddenCells[i][j];
+                rowData[i][j] = rowValues[i][j];
+                colData[j] = colValues[j];
+                count = pressed;
+            }
+        }
+    }
+    
+    /**
+     * Sets the value for cell
+     * @param value The new value of the cell
+     * @param row The row of the cell
+     * @param column The column of the cell
+     */
+    @Override
+    public void setValueAt(Object value, int row, int column) {
+       if (rowData[row][column] == forbidden[row][column]){
+           // Do nothing if its a forbidden cell
+       }
+       else if (forbidden[row][column] == "Show"){
+           rowData[row][column] = value;
+           fireTableCellUpdated(row, column);
+       }
+       else{
+           // Otherwise set its new value and update the cell
+           rowData[row][column] = value;
+           fireTableCellUpdated(row, column);
+       }
+    }
+
+    /**
+     * Indicates whether the cell is editable or not
+     * @param row
+     * @param column
+     * @return boolean value indicating if cell is editable
+     */
+    @Override
+    public boolean isCellEditable(int row, int column) { 
+        // If cell is forbidden, its not editable
+        if (count > 0 )
+            return false;
+        else if (rowData[row][column] == forbidden[row][column] ){
+            return false; 
+        }else if (rowData[row][column] != ""){
+            return false;
+        }
+       // else if(forbidden[row][column] == "Show"){
+        //    return false;
+        //}
+        else
+            // If cell is not forbidden, allow it to be editable
+            return true;
+    }
+    
+    /**
+     * Gets the number of rows in the table
+     * @return number of rows
+     */
+    @Override
+    public int getRowCount() {
+        return rowData.length;
+    }
+
+    /**
+     * Gets the number of columns in the table
+     * @return number of columns
+     */
+    @Override
+    public int getColumnCount() {
+        return colData.length;
+    }
+
+    /**
+     * Gets the value of the cell
+     * @param rowIndex The row in the table
+     * @param columnIndex The column in the table
+     * @return value of the cell
+     */
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+         return rowData[rowIndex][columnIndex];
+    }
+}
